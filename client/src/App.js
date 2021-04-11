36fdf8481f0cbe52fbbde52f44e38cdc55e8cc64
jenry valdes <jenryval93@gmail.com>
2021-04-10 20:57:33 -0500

    added server JS and authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,48 @@
 import './App.css';
-import Login from '../src/components/Login/Login'
+import React, { useState } from 'react'
+// import Login from '../src/components/Login/Login'
 
 
 function App() {
+  const [registerUsername, setRegisterUsername] = useState('')
+  const [registerPassword, setRegisterPassword] = useState('')
+  const [loginUsername, setLoginUsername] = useState('')
+  const [loginPassword, setLoginPassword] = useState('')
+  const register = () => {};
+  const login = () => {};
+  const getUser = () => {};
+
   return (
-    <div>
-      <Login />
+
+    <div className='App'>
+      <div>
+        <h1>Register</h1>
+        <input placeholder='username' onChange={e => setRegisterUsername(e.target.value)}/>
+        <input placeholder='password' onChange={e => setRegisterPassword(e.target.value)}/>
+        <button onclick={register}>submit</button>
+      </div>
+
+      <div>
+        <h1>Login</h1>
+        <input placeholder='username' onChange={e => setLoginUsername(e.target.value)}/>
+        <input placeholder='password' onChange={e => setLoginPassword(e.target.value)}/>
+        <button onclick={login}>submit</button>
+      </div>
+
+      <div>
+        <h1>Get User</h1>
+        <button onclick={getUser}>submit</button>
+      </div>
+
+
+
     </div>
+    
+
+
+
+
+
   );
 }
 
--- server.js
+++ server.js
@@ -1,29 +1,67 @@
 const mongoose = require('mongoose')
 const express = require("express");
-const path = require("path");
-const apiRoutes = require('./routes/api-routes');
+const cors = require('cors')
+const passport = require('passport')
+const passportLocal = require('passport-local').Strategy
+const cookieParser = require('cookie-parser')
+const bcrypt = require('bcryptjs')
+const session = require('express-session')
+const bodyParser = require('body-parser')
+// const path = require("path");
+// const apiRoutes = require('./routes/api-routes');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/powerx");
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors({
+  origin: "http://localhost:3000", //location of the react app were connecting to
+  credentials: true
+}))
 
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+app.use(session({
+  secret: 'secretcodoe',
+  resave: true,
+  saveUninitialized: true,
+}))
 
-// Define API routes here
-app.use(apiRoutes)
+app.use(cookieParser('secretcode'))
 
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+
+//Routes
+app.post('/login',(req,res) => {
+  console.log(req.body)
+})
+
+app.post('/register',(req,res) => {
+  console.log(req.body)
+})
+
+app.get('/user',(req,res) => {
+  console.log(req.body)
+})
+
+
+//Starter code 
+  // app.use(express.urlencoded({ extended: true }));
+  // app.use(express.json());
+
+  // Serve up static assets (usually on heroku)
+  // if (process.env.NODE_ENV === "production") {
+  //   app.use(express.static("client/build"));
+  // }
+
+  // Define API routes here
+  // app.use(apiRoutes)
+
+  // Send every other request to the React app
+  // Define any API routes before this runs
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  // });
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
