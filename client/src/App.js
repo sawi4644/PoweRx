4f00c0d5a2d250e9064719ead2a0876c18ed0c48
jenry valdes <jenryval93@gmail.com>
2021-04-11 21:15:55 -0500

    added card and CSS from bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ function App() {
     <div className='App'>
 
       <Homepage />
+      
 
 
       {/* <Register />
--- client/src/pages/Homepage/Homepage.js
+++ client/src/pages/Homepage/Homepage.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Jumbotron from 'react-bootstrap/Jumbotron';
+import HomePageCard from './HomePageCards'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 // import GetUser from './components/GetUser/getUser'
 
@@ -8,22 +10,20 @@ import Button from 'react-bootstrap/Button';
 function Homepage() {
 
     return (
+        <div>
             <Jumbotron>
                 <h1>Welcome {  }</h1>
                 <p>
                      PowerRx
                 </p>
-
                 <p>
                     Simpler way to fillout Rx forms, the easy step every dentist needs!
                 </p>
-           
             </Jumbotron>
+           
+            <HomePageCard />
+        </div>
     )
-        
-
-
-
 }
 
 export default Homepage
