06b0b0ebfce4d5ee7c3c520509596386f34e7672
Michael Rosa-Vourazeris <mrosavourazeris@gmail.com>
2021-04-12 17:43:17 -0500

    Created a new page for the RxForm

---
+++ client/src/pages/RxForm/rxForm.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Wrapper from '../../components/Wrapper/wrapper'
+
+const RxForm = () => {
+
+
+  return (
+    <Wrapper>
+      
+    </Wrapper>
+  )
+
+}
+
+export default RxForm
