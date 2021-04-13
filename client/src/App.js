952d7a7b48b4884f9c164f5e11abfc0379532e94
Michael Rosa-Vourazeris <mrosavourazeris@gmail.com>
2021-04-12 20:55:18 -0500

    Rendering HTML and checkboxes on RxForm page.

--- client/src/App.js
+++ client/src/App.js
@@ -8,30 +8,31 @@ import {
 
 import Register from './pages/Register/register'
 import Login from './pages/Login/login'
+import RxForm from './pages/RxForm/rxForm'
 
 
 
 function App() {
       
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route path="/register">
-            <Register/>
-          </Route>
-          <Route path="/login">
-            <Login/>
-          </Route>
-          <Route path="/">
-            <Register/>
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-    // <div className='App'>
-    //   <LoginPage />
-    // </div>
+    // <Router>
+    //   <div>
+    //     <Switch>
+    //       <Route path="/register">
+    //         <Register/>
+    //       </Route>
+    //       <Route path="/login">
+    //         <Login/>
+    //       </Route>
+    //       <Route path="/">
+    //         <Register/>
+    //       </Route>
+    //     </Switch>
+    //   </div>
+    // </Router>
+    <div className='App'>
+      <RxForm />
+    </div>
   );
 }
 
---
+++ client/src/pages/RxForm/sampleData.js
@@ -0,0 +1,16 @@
+const sampleData = {
+  "All Porcelain": {
+    ListItems: {
+      "Tech Recommendation": false,
+      "Layered EMAX": true,
+      "Mono EMAX": false,
+      "Layered Zirconia": false,
+      "Mono Zirconia": true,
+      "Composite": false
+    },
+    LabTime: "10 days"
+  }
+  
+}
+
+export default sampleData
