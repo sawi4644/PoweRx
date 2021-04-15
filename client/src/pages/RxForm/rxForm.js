8393f1a142296a4251ffe535d73a3e41660cb287
Michael Rosa-Vourazeris <mrosavourazeris@gmail.com>
2021-04-14 22:07:37 -0500

    Made a column component.

--- client/src/pages/RxForm/rxForm.js
+++ client/src/pages/RxForm/rxForm.js
@@ -8,9 +8,21 @@ import API from '../../utils/API'
 
 const RxForm = () => {
 
-  console.log(sampleData)
+  const [form, setForm] = useState({
+    label: {
+      name: '',
+      value: false,
+      inputType: ''
+    }
+  })
 
-  const [form, setForm] = useState(sampleData)
+  const checkUpdate = (e) => {
+    setForm({
+      ...form,
+      value: true
+    })
+    console.log(form)
+  }
 
   const save = (e) => {
     e.preventDefault()
@@ -25,7 +37,7 @@ const RxForm = () => {
   return (
     <Wrapper>
       <Form>
-        <Form.Group controlId="formBasicEmail">
+        {/* <Form.Group controlId="formBasicEmail">
           <Form.Label>Shade</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
           <Form.Text className="text-muted">
@@ -36,23 +48,13 @@ const RxForm = () => {
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Stump Shade</Form.Label>
           <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
+        </Form.Group> */}
         <Form.Group controlId="formBasicCheckbox">
           {Object.entries(sampleData).map((sData) => {
-            const [
-              key,
-              obj,
-            ] = sData
-
-            console.log(sData)
-            return(
-            <Form.Check key={key} type="checkbox" label={obj.name} />
-          )})}
-          {/* <Form.Check type="checkbox" label="Layered EMAX" />
-          <Form.Check type="checkbox" label="Mono EMAX" />
-          <Form.Check type="checkbox" label="Layered Zirconia" />
-          <Form.Check type="checkbox" label="Mono Zirconia" />
-          <Form.Check type="checkbox" label="Composite" /> */}
+            const [key, obj] = sData;
+            console.log(sData);
+            return <Form.Check key={key} type="checkbox" label={obj.name} onClick={checkUpdate} defaultValue="defaultChecked"/>;
+          })}
         </Form.Group>
         <Button variant="primary" type="submit" onClick={save}>
           Submit
