49205ecf90b1aebf760347b330c4122e9bbaed11
Zachary Jayes <zacjayes@gmail.com>
2021-04-15 19:35:12 -0500

    api-routes done

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,8 +5,12 @@ const userController = require('../controllers/userController')
 
 
 router.route('/api/rxForms')
-  .get(rxController.getRX) //get all forms
-  .post(rxController.createRx) //create a drawing- run controller model when one of these routes get hit
+  .get(rxController.getRxForm) //get all forms
+  .post(rxController.createRxForm) //create a drawing- run controller model when one of these routes get hit
+
+router.route('/api/rxForms/:id')
+.put(rxController.updateRxForm) 
+.delete(rxController.deleteRxForm)
 
 router.route('/api/users/:id')
   .get(userController.getUser)
