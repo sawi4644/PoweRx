ca077b58b12c4be63a856d91c8d87b901262d364
Michael Rosa-Vourazeris <mrosavourazeris@gmail.com>
2021-04-19 23:00:15 -0500

    Working on update profile component.

--- client/src/components/ForgotPassword/ForgotPassword.js
+++ client/src/components/ForgotPassword/ForgotPassword.js
@@ -1,17 +1,18 @@
 import React, { useRef, useState } from 'react'
 import { Form, Button, Card, Alert } from 'react-bootstrap'
 import { useAuth } from '../../contexts/AuthContext'
-import { Link, useHistory } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 
 
 export default function ForgotPassword() {
 
   const emailRef = useRef()
-  const { login } = useAuth()
+  const { resetPassword } = useAuth()
   const [error, setError] = useState('')
+  const [message, setMessage] = useState('')
   const [loading, setLoading] = useState(false)
-  const history = useHistory()
+  
 
   async function handleSubmit(e) {
     e.preventDefault()
@@ -19,12 +20,13 @@ export default function ForgotPassword() {
     
 
     try {
+      setMessage('')
       setError('')
       setLoading(true)
-      await login(emailRef.current.value, passwordRef.current.value)
-      history.push("/")
+      await resetPassword(emailRef.current.value)
+      setMessage('Check your inbox for further instructions')
     } catch {
-      setError('Failed to sign in')
+      setError('Failed reset password')
     }
 
     setLoading(false)
@@ -36,8 +38,9 @@ export default function ForgotPassword() {
     <>
       <Card>
         <Card.Body>
-          <h2 className="text-center mb-4">Log In</h2>
+          <h2 className="text-center mb-4">Password Reset</h2>
           {error && <Alert variant="danger">{error}</Alert>}
+          {message && <Alert variant="success">{message}</Alert>}
           <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
@@ -45,6 +48,9 @@ export default function ForgotPassword() {
             </Form.Group>
             <Button className="w-100" type="submit" disabled={loading}>Reset Password</Button>
           </Form>
+          <div className="w-100 text-center mt-3">
+            <Link to='/login'>Log In</Link>
+          </div>
         </Card.Body>
       </Card>
       <div className="w-100 text-center mt-2">
--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -24,6 +24,10 @@ export function AuthProvider({ children}) {
     return auth.signOut()
   }
 
+  function resetPassword(email){
+    return auth.sendPasswordResetEmail(email)
+  }
+
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       setCurrentUser(user);
@@ -37,6 +41,7 @@ export function AuthProvider({ children}) {
     currentUser,
     login,
     logout,
+    resetPassword,
     signup
   }
 
