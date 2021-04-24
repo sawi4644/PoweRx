f489d41f159d31f3ab90de45533b5311a51121cd
Zachary Jayes <zacjayes@gmail.com>
2021-04-24 14:09:52 -0500

    history and mycard

--- client/src/components/History/History.js
+++ client/src/components/History/History.js
@@ -9,25 +9,18 @@ import {useHistory } from "react-router-dom";
 import { useAuth } from '../../contexts/AuthContext'
 import MyCard from '../Card/MyCard'
 
-
-
-
 const RxHistory = () => {
-
   let history = useHistory();
   const { currentUser, createToken } = useAuth()
-  
-
   const [filledForm, setFilledForm] = useState([]);
-
     useEffect(() => {
        RxHistoryForm()
     }, [])
-    
     const RxHistoryForm = e => {
         createToken().then(headers => {
           API.getHistory(currentUser.uid, headers)
           .then(response => {
+          console.log(response.data)
           setFilledForm(response.data)
         })
         .catch(err => console.log(err))
@@ -36,18 +29,13 @@ const RxHistory = () => {
   return (
     <div>
       {/* <pre>{JSON.stringify(filledForm, null, 2)}</pre> */}
-     
       {filledForm.map((oneForm) => {
         return(
           <MyCard {...oneForm}></MyCard>
         )
-
       })}
     </div>
-
-    
   );
-
 }
 
 export default RxHistory
