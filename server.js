6635255aa229a8bbe6dcdb9876c9ce10fdc1af02
jenry valdes <jenryval93@gmail.com>
2021-04-25 10:14:45 -0500

    adding code to heroku

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const User = require('./models/user')
 const morgan = require('morgan')
-
+console.log(process.env.MONGODB_URI)
 mongoose.connect(process.env.MONGODB_URI, {
 // mongoose.connect("mongodb://localhost/powerrx", {
   useNewUrlParser: true,
